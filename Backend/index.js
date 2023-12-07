a582b5028f28a88ce3e1f581ddd7dfd6e43d10e9
Binitstha <binitcr336@gmail.com>
2023-12-07 23:51:29 +0545

    code checked

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
-const registerRoute = require('./userRegister/register.js');
-const forgetRoute = require('./forgetPassword/forgetPassword.js')
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const registerRoute = require("./controller/routes/userRegister/register.js");
+const forgetRoute = require("./controller/routes/forgetPassword/forgetPassword.js");
+const resetRoute = require("./controller/routes/forgetPassword/codeChecker.js");
+
 app.use(express.json());
-app.use('/', registerRoute)
-app.use('/', forgetRoute)
+app.use("/", registerRoute);
+app.use("/", forgetRoute);
+app.use("/", resetRoute);
 
-
-const sequelize = require("./config/config")
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const sequelize = require("./config/config");
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
