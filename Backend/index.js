5f2f105fa82d9180514d045d1ca3d8c3c32cdc65
Binitstha <binitcr336@gmail.com>
2023-12-08 15:16:36 +0545

    comment removed

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-// import registerRoute from "./controller/routes/userRegister/register.js";
-// import forgetRoute from "./controller/routes/forgetPassword/forgetPassword.js";
-// import resetRoute from "./controller/routes/forgetPassword/codeChecker.js";
+import registerRoute from "./controller/routes/userRegister/register.js";
+import forgetRoute from "./controller/routes/forgetPassword/forgetPassword.js";
+import resetRoute from "./controller/routes/forgetPassword/codeChecker.js";
 import student_dataRoute from "./controller/routes/student_data/student_data.js";
 
-// app.use("/", registerRoute);
-// app.use("/", forgetRoute);
-// app.use("/forgetPassword", resetRoute);
+app.use("/", registerRoute);
+app.use("/", forgetRoute);
+app.use("/forgetPassword", resetRoute);
 app.use("/", student_dataRoute);
 
 
