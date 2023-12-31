e78bfb8085e70d296933cfcdfbb79b105b79dda0
Binitstha <binitcr336@gmail.com>
2023-12-31 17:16:43 +0545

    data deletetion in status

--- Backend/controller/routes/QuestionsPapers/QuestionEntry.js
+++ Backend/controller/routes/QuestionsPapers/QuestionEntry.js
@@ -5,6 +5,7 @@ import path, { dirname, join } from 'path'; // Import 'dirname' and 'join' from 
 import multer from 'multer';
 import fs from 'fs';
 import cors from 'cors'
+import { error } from 'console';
 
 const router = express.Router()
 const storage = multer.diskStorage({
@@ -20,39 +21,39 @@ const storage = multer.diskStorage({
 router.use(cors())
 const upload = multer({ storage: storage })
 
-router.put('/questionEntry',upload.single('image'), async (req, res) => {
+router.put('/questionEntry', upload.single('image'), async (req, res) => {
     const userInfo = await req.body;
-    console.log("file",req.file)
-    const imageBuffer = req.file? fs.readFileSync(req.file.path):null
+    console.log("file", req.file)
+    const imageBuffer = req.file ? fs.readFileSync(req.file.path) : null
     try {
         await questionDB.create({
             question: userInfo.question,
             year: userInfo.year,
             semester: userInfo.semester,
             subject_code: userInfo.subject_code,
-            mark : userInfo.mark,
-            image : imageBuffer
+            mark: userInfo.mark,
+            image: imageBuffer
         });
         res.status(200).send("data inserted")
     }
-    
+
     catch (error) {
-         if (error.parent.code == 'ER_DUP_ENTRY') {
+        if (error.parent.code == 'ER_DUP_ENTRY') {
             console.error('Duplicate entry error:', error.message);
             res.status(400).json({ error: 'Duplicate entry error', message: error.message });
-          } 
-          else {
+        }
+        else {
             console.error('Error:', error.message);
             console.log("error code", error.code)
             res.status(500).json({ error: 'Internal Server Error', message: error.message });
-          }
+        }
     }
 })
 router.post('/questionDelete', async (req, res) => {
     try {
         const userInfo = await req.body;
 
-        await questionDB.destroy({
+        const result = await questionDB.destroy({
             where: {
                 question: userInfo.question,
                 year: userInfo.year,
@@ -61,7 +62,12 @@ router.post('/questionDelete', async (req, res) => {
                 mark: userInfo.mark,
             }
         });
-        res.status(200).send("Deleted")
+        if (result === 0) {
+            res.status(404).send("Data not found");
+        }
+        else {
+            res.status(200).send("Deleted")
+        }
     }
 
     catch (error) {
