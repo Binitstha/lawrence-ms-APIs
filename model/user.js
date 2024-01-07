8eb30b57a7762338c686433cb76a00668bc8bf8f
Binitstha <binitcr336@gmail.com>
2024-01-07 22:09:03 +0545

    .

--- model/user.js
+++ model/user.js
@@ -1,58 +1,58 @@
-import { Sequelize,DataTypes } from "sequelize";
-const sequelize= new Sequelize('sql12674468','sql12674468','ZyKhdG9tz6',{
-	dialect:'mysql',
-	host:'sql12.freemysqlhosting.net',
+import { Sequelize, DataTypes } from "sequelize";
+const sequelize = new Sequelize('sql12674468', 'sql12674468', 'ZyKhdG9tz6', {
+	dialect: 'mysql',
+	host: 'sql12.freemysqlhosting.net',
 });
 
-async function sequelizeAuth(){
-	try{
+async function sequelizeAuth() {
+	try {
 		await sequelize.authenticate();
 		console.log("Connected to students database!");
-	} catch(error){
+	} catch (error) {
 		console.log(error);
 	}
 }
 
 sequelizeAuth();
 
-const user=sequelize.define('user',{
-	id:{
+const user = sequelize.define('user', {
+	id: {
 		type: DataTypes.BIGINT,
-		allowNull:false,
-		primaryKey:true
+		allowNull: false,
+		primaryKey: true
 	},
-	email:{
+	email: {
 		type: DataTypes.STRING,
 		allowNull: false,
 	},
 	password: {
 		type: DataTypes.STRING,
-		allowNull:false,
+		allowNull: false,
 	},
-	name:{
+	name: {
 		type: DataTypes.STRING,
-		allowNull:false,
+		allowNull: false,
 	},
-	role:{
-		type:DataTypes.STRING,
-		allowNull:false
+	role: {
+		type: DataTypes.STRING,
+		allowNull: false
 	},
-	contact:{
-		type:DataTypes.BIGINT,
-		allowNull:false
+	contact: {
+		type: DataTypes.BIGINT,
+		allowNull: false
 	},
-	age:{
-		type:DataTypes.INTEGER,
-		allowNull:false,
+	age: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
 
 	},
-	address:{
-		type:DataTypes.STRING,
-		allowNull:false
+	address: {
+		type: DataTypes.STRING,
+		allowNull: false
 	}
-},{
-	timestamps:false,
-	tableName:'users',
-	
+}, {
+	timestamps: false,
+	tableName: 'users',
+
 });
 export default user;
