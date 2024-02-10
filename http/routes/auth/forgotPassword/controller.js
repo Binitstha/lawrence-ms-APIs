490c1b009852b09430441c6199d67e8d44e51523
Binitstha <binitcr336@gmail.com>
2024-02-10 22:44:18 +0545

    reset password complete

--- http/routes/auth/forgotPassword/controller.js
+++ http/routes/auth/forgotPassword/controller.js
@@ -1,25 +1,54 @@
 import { createTransport } from "nodemailer";
-
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-
-
-let genratedCode;
-
-export const getEmail = async (req) => {
-    return req.body.email;
-};
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const emailChecker = async (req, res) => {
     const result = await req.body
 
-    if (await prisma.user.findFirst({
+    const user = await prisma.user.findFirst({
         where: {
             email: result.email
         }
-    })) {
+    })
+    if (user) {
+
         console.log(result)
         res.status(200).send({ "result": result, message: "Email found" })
+        const transporter = createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "2e021b62f3788b",
+                pass: "2a86060576a97d",
+            },
+        });
+        const payload = JSON.stringify({
+            id: user.id,
+            email: user.email,
+        });
+        const secretKey = process.env.SECRET_KEY;
+        const token = jwt.sign(payload, secretKey);
+
+        storeToken(user, token)
+
+        const resetLink = `http://localhost:4321/forgotPassword/resetPassword?token=${token}`;
+
+        transporter.sendMail({
+            from: 'binitcr336@gmail.com',
+            to: "imbinit54@gmail.com",
+            subject: 'Password Reset',
+            html: `Click <a href="${resetLink}">here</a> to reset your password.`
+        }, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                res.status(500).send('Error sending email');
+            } else {
+                console.log('Email sent:', info.response);
+                res.send('Password reset email sent');
+            }
+        });
     }
     else {
         res.send({
@@ -29,74 +58,60 @@ export const emailChecker = async (req, res) => {
     }
 }
 
-export const forgetPasswordController = async (req, res) => {
-    console.log("forgetRequest");
-    let reqEmail = await getEmail(req);
-    try {
-        const databaseEmail = await prisma.user.findMany({
+const storeToken = async (user, token) => {
+    if (
+        prisma.resetPassToken.findFirst({
             where: {
-                email: reqEmail,
+                id: user.id,
+            }
+        })) {
+        return await prisma.resetPassToken.update({
+            data: {
+                token: token,
             },
-        });
-
-        if (databaseEmail.length > 0 && databaseEmail[0].email) {
-            console.log("found");
-            res.send("email found");
-        }
-
-        await codeGenerator();
-        await emailSend();
-    } catch (error) {
-        console.log(error);
-        return res.send("user not found");
+            where: {
+                id: user.id
+            }
+        })
     }
-}
-
-export const codeGenerator = () => {
-    const codeArray = [];
-    for (let i = 0; i < 5; i++) {
-        const radNum = Math.floor(Math.random() * 10);
-        codeArray.push(radNum);
-    }
-    try {
-        if (codeArray.length == 5) {
-            genratedCode = codeArray.reduce(
-                (acc, currentNum) => acc * 10 + currentNum,
-                0
-            );
-        }
-    } catch (error) {
-        throw error;
-    }
-    return genratedCode;
-}
-
-
-
-export const emailSend = async () => {
-    const transporter = createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: "2e021b62f3788b",
-            pass: "2a86060576a97d",
+    return await prisma.resetPassToken.create({
+        data: {
+            id: user.id,
+            token: token,
         },
     });
-
-    const mailOptions = {
-        from: "imbinit54@gmail.com",
-        to: "binitcr336@gmail.com",
-        subject: "Code for forgetted password",
-        text: `${genratedCode}
-            code for setting new password`,
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
 }
 
+export const resetPassword = async (req, res) => {
+    console.log("forgetRequest");
+    const password = await req.body.password;
+    const token = await req.headers.authorization.split(' ')[1]
+    const secretKey = process.env.SECRET_KEY
+    const decodedToken = jwt.verify(token, secretKey)
+    const userEmail = decodedToken.email
+    const userId = decodedToken.id
+    try {
+
+        prisma.user.update({
+            data: {
+                password: await bcrypt.hash(password, 12),
+            },
+            where: {
+                id: userId,
+                email: userEmail
+            }
+        }).then(data => {
+            res.send({
+                status: 200,
+                data: data,
+                message: "Password reset successfully"
+            })
+        })
+    }
+    catch (err) {
+        res.status(500).send({
+            status: 500,
+            message: "Failed"
+        })
+    }
+}
--- http/routes/auth/forgotPassword/routes.js
+++ http/routes/auth/forgotPassword/routes.js
@@ -1,19 +1,18 @@
 import express from 'express';
-const router=express.Router();
-import { validateCode } from './validator.js';
-import { forgetPasswordController } from './controller.js';
+const router = express.Router();
+import { resetPassword } from './controller.js';
 import { emailChecker } from './controller.js';
 
-router.post('/emailCheck', async(req,res)=>{
-    emailChecker(req,res)
+router.post('/emailCheck', async (req, res) => {
+    emailChecker(req, res)
 })
 
-router.post('/forgetPassword', async (req, res) => {
-    forgetPasswordController(req,res);
+router.post('/resetPassword', async (req, res) => {
+    resetPassword(req, res);
 });
 
-router.post('/resetPassword',(req,res)=>{
-    validateCode(req,res);
-})
+// router.post('/resetPassword',(req,res)=>{
+//     validateCode(req,res);
+// })
 
 export default router;
